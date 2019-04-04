310c0ee42950811705a2cec7cd46739cedff53b6
Kimberly Ramos <ramosk013@gmail.com>
2019-04-04 12:49:23 -0500

    Added more GUI attributes:   Game Type Button   Timer Input Changed orientation of game board Need to add functionality to board game buttons ( or change to user input)

--- src/sample/BoardGUI.java
+++ src/sample/BoardGUI.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -20,6 +21,8 @@ public class BoardGUI extends Stage{
     Button computerHouseButton;
     Button playerJarButton;
     Button computerJarButton;
+
+    Button backButton = new Button("Back to Main Menu");
     Label label = new Label("Game Board");
 
 
@@ -29,11 +32,8 @@ public class BoardGUI extends Stage{
     GridPane grid = new GridPane();
     Scene scene = new Scene(root, 900, 900);
 
-    void updateButtonLabels(){
 
-    }
-
-    public BoardGUI(int numHouses, int numMarbles, boolean randDistMarbles){
+    public BoardGUI(int numHouses, int numMarbles, boolean randDistMarbles, boolean gameType, long timerLength){
         //set scene properties
         grid.setPadding(new Insets(30,80,50,80));
         grid.setVgap(9);
@@ -49,27 +49,29 @@ public class BoardGUI extends Stage{
         Controller b = new Controller();
         b.numHousesINPUT = numHouses;
         b.numSeedsINPUT = numMarbles;
-        updateButtonLabels();
+        b.updateArrayLists(numHouses, numMarbles);
+        b.updateButtonLabels();
 
         //random distribution of marbles
         if(randDistMarbles) {
             b.assignRandomMarbles(); //TODO: remove print statements
+            b.getBoardStatus();
+            b.updateButtonLabels();
         }
-            //b.getBoardStatus(); //TODO: remove later
-            updateButtonLabels();
-
 
 
         //button board //TODO: FIX ME
         for( int i = 0; i < numHouses; i++){
             computerHouseButton = new Button();
             computerHouseButton.setText(Integer.toString(b.computer.get(i).numMarbles));
-            GridPane.setConstraints(computerHouseButton,i+1,2);
+            GridPane.setConstraints(computerHouseButton,1,i+2);
+            computerHouseButton.setMinSize(60,60);
             grid.getChildren().add(computerHouseButton);
 
             playerHouseButton = new Button();
             playerHouseButton.setText(Integer.toString(b.player.get(i).numMarbles));
-            GridPane.setConstraints(playerHouseButton,i+1,3);
+            GridPane.setConstraints(playerHouseButton,2,i+2);
+            playerHouseButton.setMinSize(60,60);
             grid.getChildren().add(playerHouseButton);
 
         }
@@ -77,23 +79,50 @@ public class BoardGUI extends Stage{
         //player jar
         playerJarButton = new Button();
         playerJarButton.setText(Integer.toString(b.jars.get(0).numMarbles));
-        GridPane.setConstraints(playerJarButton,0,2);
-        GridPane.setRowSpan(playerJarButton,2);
+        GridPane.setConstraints(playerJarButton,1,1);
+        GridPane.setColumnSpan(playerJarButton,2);
+        playerJarButton.setMinSize(130,60);
 
         //computer jar
         computerJarButton = new Button();
         computerJarButton.setText(Integer.toString(b.jars.get(1).numMarbles));
-        GridPane.setConstraints(computerJarButton,numHouses+2,2);
-        GridPane.setRowSpan(computerJarButton,2);
+        GridPane.setConstraints(computerJarButton,1,numHouses + 2);
+        GridPane.setColumnSpan(computerJarButton,2);
+        computerJarButton.setMinSize(130,60);
 
+
+        //Back Button
+        GridPane.setConstraints(backButton,15,15);
+        GridPane.setRowSpan(backButton,2);
+        GridPane.setColumnSpan(backButton,5);
 
 
         /*ADD ATTRIBUTES TO SCENE*/
         grid.getChildren().add(label);
         grid.getChildren().add(playerJarButton);
         grid.getChildren().add(computerJarButton);
+        grid.getChildren().add(backButton);
 
         this.show();
+
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //mainSceneWindow.show();
+            }
+        });
+
+        computerHouseButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                /*
+                * if computers turn
+                *   get index of button, get numOfMarbles (label),
+                *   set numOfMarbles (label) to 0
+                * else
+                * nothing*/
+            }
+        });
     }
 
 }
